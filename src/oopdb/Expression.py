cea49a70bdf8459e460cc29a132905a41b880b55
Basicula <maksim.basum@gmail.com>
2021-12-04 16:42:57 +0200

    Add code documentation

--- src/oopdb/Expression.py
+++ src/oopdb/Expression.py
@@ -3,6 +3,25 @@ import enum
 import copy
 
 class Operation(enum.Enum):
+    '''
+    Supported operations
+
+    Each operation defines value type
+        EQUAL
+        GREATER_THAN
+        LESS_THAN
+        GREATER_THAN_OR_EQUAL
+        LESS_THAN_OR_EQUAL
+        NOT_EQUAL
+            Supported any value type (str, int etc) that have to match column type
+        BETWEEN
+            Value type - tuple with 2 components, each component must have same type and match column type
+        LIKE
+            Value type - string(str)
+        IN
+            Value type - list
+    '''
+
     EQUAL = "="
     GREATER_THAN = ">"
     LESS_THAN = "<"
@@ -15,10 +34,20 @@ class Operation(enum.Enum):
 
 class Expression:
     '''
+    Abstraction for bool expression that can be used in database queries
     '''
 
     def __init__(self, column_name : str, operation : Operation, value : Any) -> None:
         '''
+        Creates expression after check whether the value type matches given operation,
+        if not Exception will be raised
+
+        column_name : str, required
+            The column name that we want to filter out with given value and operation
+        operation : Operation, required
+            The operation to be applied. Operation defines value type see details in Operation enum
+        value : Any, required
+            The value for filtering. Its type depends on the given operation
         '''
         Expression.__check_value_type_for_operation(operation, value)
         self.expression = f"{column_name} {operation.value} "
@@ -34,17 +63,27 @@ class Expression:
 
     def OR(self, expression : 'Expression') -> 'Expression':
         '''
+        Creates new expression that combines two other expressions with the 'or'
         '''
         return Expression.__binary_operation(self, expression, "OR")
 
     def AND(self, expression : 'Expression') -> 'Expression':
         '''
+        Creates new expression that combines two other expressions with the 'and'
         '''
         return Expression.__binary_operation(self, expression, "AND")
 
     @staticmethod
     def __binary_operation(left_expression : 'Expression', right_expression : 'Expression', operation : str) -> 'Expression':
         '''
+        Common function that proccess composition of two other expressions by the given operation
+
+        left_expression : Expression, required
+            Left sided part of the future composite expression
+        right_expression : Expression, required
+            Right sided part of the future composite expression
+        operation : str, required
+            String representation of the concatenate operation
         '''
         right = Expression.__get_wrapped_expression_str(right_expression)
         left = Expression.__get_wrapped_expression_str(left_expression)
@@ -55,6 +94,12 @@ class Expression:
 
     @staticmethod
     def __get_wrapped_expression_str(expression : 'Expression') -> str:
+        '''
+        Wraps expression string representation if it's composite expression
+
+        expression : Expression, required
+            Expression to be wrapped
+        '''
         res = expression.expression
         if not expression.is_simple:
             res = f"({res})"
@@ -63,6 +108,10 @@ class Expression:
     @staticmethod
     def NOT(expression : 'Expression') -> 'Expression':
         '''
+        Negation for expression
+
+        expression : Expression, required
+            Expression to be negated
         '''
         res = copy.copy(expression)
         res.expression = "NOT "
@@ -75,6 +124,11 @@ class Expression:
     @staticmethod
     def __check_value_type_for_operation(operation : Operation, value : Any) -> None:
         '''
+        Checks if the value type matches supported types by the given operation
+
+        operation : Operation, required
+        value : Any, required
+            Value to check type
         '''
         error_message = f"Value type for operation {operation.name} with given value type {type(value)} for {value}"
         ok = True
