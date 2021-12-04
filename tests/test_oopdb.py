cb19d69842ca2338be0e1ec7b11fc4dd56de1fef
Basicula <maksim.basum@gmail.com>
2021-12-04 19:20:36 +0200

    Add test for where command Small update for old tests

--- tests/test_oopdb.py
+++ tests/test_oopdb.py
@@ -1,9 +1,10 @@
 from oopdb.OOPDB import OOPDB, RowsStyle
 from oopdb.ColumnConfig import ColumnConfig, PrimaryKey, DataTypes
+from oopdb.Expression import Expression, Operation
 import unittest
 import sqlite3
 import os
-from typing import List
+from typing import Any, List
 
 class TempFileHolder:
     def __init__(self, filename : str):
@@ -28,19 +29,13 @@ def compare_files(file1 : str, file2 : str) -> bool:
         data2 = f2.read()
         return data1 == data2
 
-def add_table_to_db(db : OOPDB, table_name : str, column_names : List[str], row_cnt : int, row_content : List[str] = []) -> None:
-    columns = []
-    for column_name in column_names:
-        columns.append(ColumnConfig(column_name, DataTypes.TEXT))
+def add_table_to_db(db : OOPDB, table_name : str, columns : List[ColumnConfig], rows_content : List[List[Any]] = []) -> None:
+    column_names = []
+    for column in columns:
+        column_names.append(column.name)
     db.create_table(table_name, columns).execute()
-    for row in range(row_cnt):
-        values = []
-        if len(row_content) > 0:
-            values = row_content
-        else:
-            for col in range(len(columns)):
-                values.append(f"Content_{row}_{col}")
-        db.insert_into(table_name, column_names, values).execute()
+    for row in range(len(rows_content)):
+        db.insert_into(table_name, column_names, rows_content[row]).execute()
 
 class TestOOPDB(unittest.TestCase):
     def test_creation(self):
@@ -70,11 +65,12 @@ class TestOOPDB(unittest.TestCase):
         db = temp_db.db
 
         table_cnt = 10
+        column = ColumnConfig("Column", DataTypes.TEXT)
         expected_table_names = []
         for i in range(table_cnt):
             table_name = f"Table_{i}"
             expected_table_names.append(table_name)
-            add_table_to_db(db, table_name, ["Column"], 0)
+            add_table_to_db(db, table_name, [column])
         actual_table_names = db.table_names().fetch()
         actual_table_names = [a[0] for a in actual_table_names]
 
@@ -86,37 +82,27 @@ class TestOOPDB(unittest.TestCase):
 
         column_cnt = 10
         expected_column_names = []
+        columns = []
         for i in range(column_cnt):
-            column_name = f"Column_{i}"
-            expected_column_names.append(column_name)
+            columns.append(ColumnConfig(f"Column_{i}", DataTypes.TEXT))
+            expected_column_names.append(columns[-1].name)
         table_name = "TestTable"
-        add_table_to_db(db, table_name, expected_column_names, 0)
+        add_table_to_db(db, table_name, columns)
         actual_column_names = db.column_names(table_name).fetch()
         actual_column_names = [a[0] for a in actual_column_names]
 
         self.assertListEqual(expected_column_names, actual_column_names)
-
-    def test_data_selection_count(self):
-        temp_db = TempDB()
-        db = temp_db.db
-
-        table_name = "TestTable"
-        column_name = "TestColumn"
-        row_cnt = 123
-        add_table_to_db(db, table_name, [column_name], row_cnt)
-
-        actual_row_cnt = db.select_count(table_name).fetch()[0][0]
-        self.assertEqual(row_cnt, actual_row_cnt)
 
     def test_data_selection(self):
         temp_db = TempDB()
         db = temp_db.db
 
         table_name = "TestTable"
-        column_names = ["TestColumn"]
+        columns = [ColumnConfig("TestColumn", DataTypes.TEXT)]
         row_cnt = 123
         row_content = ["RowContent"]
-        add_table_to_db(db, table_name, column_names, row_cnt, row_content)
+        rows_content = [row_content for _ in range(row_cnt)]
+        add_table_to_db(db, table_name, columns, rows_content)
 
         actual_row_cnt = db.select_count(table_name).fetch()[0][0]
         self.assertEqual(row_cnt, actual_row_cnt)
@@ -130,10 +116,11 @@ class TestOOPDB(unittest.TestCase):
         db = temp_db.db
 
         table_name = "TestTable"
-        column_names = ["TestColumn_1", "TestColumn_2"]
+        columns = [ColumnConfig("TestColumn_1", DataTypes.TEXT), ColumnConfig("TestColumn_2", DataTypes.TEXT)]
         row_cnt = 123
         row_content = ["TestColumn_1Content_1", "TestColumn_2Content_2"]
-        add_table_to_db(db, table_name, column_names, row_cnt, row_content)
+        rows_content = [row_content for _ in range(row_cnt)]
+        add_table_to_db(db, table_name, columns, rows_content)
 
         actual_row_cnt = db.select_count(table_name).fetch()[0][0]
         self.assertEqual(row_cnt, actual_row_cnt)
@@ -141,9 +128,9 @@ class TestOOPDB(unittest.TestCase):
         actual_row_contents = db.select(table_name).fetch(RowsStyle.DICTIONARY)
         self.assertEqual(type(actual_row_contents[0]), dict)
         for actual_row_content in actual_row_contents:
-            self.assertListEqual(list(actual_row_content.keys()), column_names)
+            self.assertListEqual(list(actual_row_content.keys()), [columns[0].name, columns[1].name])
         for actual_row_content in actual_row_contents:
-            self.assertListEqual(row_content, [actual_row_content[column_names[0]], actual_row_content[column_names[1]]])
+            self.assertListEqual(row_content, [actual_row_content[columns[0].name], actual_row_content[columns[1].name]])
 
     def test_integer_type(self):
         temp_db = TempDB()
@@ -153,8 +140,9 @@ class TestOOPDB(unittest.TestCase):
         int_column = ColumnConfig("Integers", DataTypes.INTEGER, False)
         db.create_table(table_name, [int_column]).execute()
         db.insert_into(table_name, [int_column.name], [321]).execute()
-        result = db.select(table_name).fetch()[0][0]
-        self.assertEqual(type(result), int)
+        result = db.select(table_name).fetch()
+        self.assertEqual(type(result[0][0]), int)
+        self.assertEqual(result[0][0], 321)
 
     def test_bool_type(self):
         temp_db = TempDB()
@@ -176,9 +164,9 @@ class TestOOPDB(unittest.TestCase):
         db = temp_db.db
         
         table_name = "TestTable"
-        bool_column = ColumnConfig("Bools", DataTypes.BOOL, False)
         int_column = ColumnConfig("Integers", DataTypes.INTEGER, False)
         text_column = ColumnConfig("Text", DataTypes.TEXT, False)
+        bool_column = ColumnConfig("Bools", DataTypes.BOOL, False)
         column_names = [int_column.name, text_column.name, bool_column.name]
         db.create_table(table_name, [int_column, text_column, bool_column]).execute()
         db.insert_into(table_name, column_names, [123, "Test123", False]).execute()
@@ -194,6 +182,56 @@ class TestOOPDB(unittest.TestCase):
         self.assertEqual(updated_values[0][0], 321)
         self.assertEqual(updated_values[0][1], "Test321")
         self.assertEqual(updated_values[0][2], True)
+
+    def test_where(self):
+        temp_db = TempDB()
+        db = temp_db.db
+        
+        table_name = "TestTable"
+        int_column = ColumnConfig("Id", DataTypes.INTEGER, False)
+        text_column = ColumnConfig("Text", DataTypes.TEXT, False)
+        bool_column = ColumnConfig("Enable", DataTypes.BOOL, False)
+        row_cnt = 100
+        rows = []
+        for row_id in range(row_cnt):
+            rows.append((row_id, f"ThisIsRow{row_id}", bool(row_id % 2)))
+        add_table_to_db(db, table_name, [int_column, text_column, bool_column], rows)
+        
+        is_enabled = Expression(bool_column.name, Operation.EQUAL, True)
+        enabled_rows_cnt = db.select_count(table_name).where(is_enabled).fetch()[0][0]
+        self.assertEqual(enabled_rows_cnt, 50)
+        enabled_rows = db.select(table_name).where(is_enabled).fetch()
+        for row in enabled_rows:
+            self.assertTrue(bool(row[0] % 2))
+            self.assertTrue(row[2])
+
+        text_like = Expression(text_column.name, Operation.LIKE, "ThisIsRow2%")
+        regex_rows_cnt = db.select_count(table_name).where(text_like).fetch()[0][0]
+        self.assertEqual(regex_rows_cnt, 11)
+
+    def test_update_where(self):
+        temp_db = TempDB()
+        db = temp_db.db
+        
+        table_name = "TestTable"
+        int_column = ColumnConfig("Id", DataTypes.INTEGER, False)
+        text_column = ColumnConfig("Text", DataTypes.TEXT, False)
+        bool_column = ColumnConfig("Enable", DataTypes.BOOL, False)
+        row_cnt = 100
+        rows = []
+        for row_id in range(row_cnt):
+            rows.append([row_id, f"ThisIsRow{row_id}", bool(row_id % 2)])
+        add_table_to_db(db, table_name, [int_column, text_column, bool_column], rows)
+        
+        is_enabled = Expression(bool_column.name, Operation.EQUAL, True)
+        enabled_rows = db.select(table_name).where(is_enabled).fetch()
+        for row in enabled_rows:
+            self.assertTrue(bool(row[0] % 2))
+            self.assertTrue(row[2])
+
+        db.update(table_name, [bool_column.name], [False]).where(is_enabled).execute()
+        enabled_rows_cnt = db.select_count(table_name).where(is_enabled).fetch()[0][0]
+        self.assertEqual(enabled_rows_cnt, 0)
 
 if __name__ == "__main__":
     unittest.main()
