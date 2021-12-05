e1879bd31806a7baafcb777e26b37028e6918b90
Basicula <maksim.basum@gmail.com>
2021-12-05 12:50:32 +0200

    Fix tests

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -4,7 +4,7 @@ import unittest
 class TestSimpleExpression(unittest.TestCase):
     def test_equality(self):
         exp = Expression("Column", Operation.EQUAL, "123")
-        expected_expression = "Column = 123"
+        expected_expression = "Column = '123'"
         self.assertEqual(exp.expression, expected_expression)
 
     def test_less(self):
@@ -24,12 +24,12 @@ class TestSimpleExpression(unittest.TestCase):
 
     def test_less_or_equal(self):
         exp = Expression("Column", Operation.LESS_THAN_OR_EQUAL, "123")
-        expected_expression = "Column <= 123"
+        expected_expression = "Column <= '123'"
         self.assertEqual(exp.expression, expected_expression)
 
     def test_not_equal(self):
         exp = Expression("Column", Operation.NOT_EQUAL, "123")
-        expected_expression = "Column <> 123"
+        expected_expression = "Column <> '123'"
         self.assertEqual(exp.expression, expected_expression)
 
     def test_in(self):
@@ -61,7 +61,7 @@ class TestSimpleExpression(unittest.TestCase):
 
     def test_not(self):
         exp_not = Expression.NOT(Expression("ColumnNot", Operation.EQUAL, "123"))
-        expected_expression = "NOT ColumnNot = 123"
+        expected_expression = "NOT ColumnNot = '123'"
         self.assertEqual(exp_not.expression, expected_expression)
 
 class TestCompositeExpression(unittest.TestCase):
@@ -70,7 +70,7 @@ class TestCompositeExpression(unittest.TestCase):
         exp2 = Expression("Column2", Operation.IN, [1,2,3,5])
         exp3 = Expression("Column3", Operation.LESS_THAN, 50)
         exp4 = exp1.OR(exp2)
-        expected_expression = "Column1 = 123 OR Column2 IN (1, 2, 3, 5)"
+        expected_expression = "Column1 = '123' OR Column2 IN (1, 2, 3, 5)"
         self.assertEqual(exp4.expression, expected_expression)
         exp5 = exp4.OR(exp3)
         expected_expression = f"({expected_expression}) OR Column3 < 50"
@@ -81,7 +81,7 @@ class TestCompositeExpression(unittest.TestCase):
         exp2 = Expression("Column2", Operation.BETWEEN, (1,5))
         exp3 = Expression("Column3", Operation.LIKE, "a%")
         exp4 = exp1.AND(exp2)
-        expected_expression = "Column1 > 123 AND Column2 BETWEEN 1 AND 5"
+        expected_expression = "Column1 > '123' AND Column2 BETWEEN 1 AND 5"
         self.assertEqual(exp4.expression, expected_expression)
         exp5 = exp4.AND(exp3)
         expected_expression = f"({expected_expression}) AND Column3 LIKE 'a%'"
@@ -92,7 +92,7 @@ class TestCompositeExpression(unittest.TestCase):
         exp2 = Expression("Column2", Operation.LESS_THAN_OR_EQUAL, "321")
         exp3 = Expression("Column3", Operation.NOT_EQUAL, "test")
         exp = Expression.NOT(exp3.AND(exp1.OR(exp2)))
-        expected_expression = "NOT (Column3 <> test AND (Column1 >= 123 OR Column2 <= 321))"
+        expected_expression = "NOT (Column3 <> 'test' AND (Column1 >= '123' OR Column2 <= '321'))"
         self.assertEqual(exp.expression, expected_expression)
 
     def test_all_in_one(self):
